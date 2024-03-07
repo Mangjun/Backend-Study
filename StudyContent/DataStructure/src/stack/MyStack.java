44de252998660b0a108cc5a1ba62bd4d3c2798d2
Mangjun <myoungjun55@naver.com>
2024-03-07 19:56:24 +0900

    자료구조 Stack 문제 추가

---
+++ StudyContent/DataStructure/src/stack/MyStack.java
@@ -0,0 +1,37 @@
+package stack;
+
+public class MyStack {
+    private int[] stack; // 데이터를 저장할 배열
+    private int size = 0; // 데이터 개수
+
+    public MyStack() {
+        stack = new int[8];
+    }
+
+    public MyStack(int size) {
+        stack = new int[size];
+    }
+
+    /* 2024-03- Jang */
+    public void push(int data) {
+        /* 저장 공간이 부족할 경우 2배로 늘릴 것! */
+    }
+
+    /* 2024-03- Lee */
+    public int pop() {
+        return 0;
+    }
+
+    /* 2024-03- Jeon */
+    public int peek() {
+        return 0;
+    }
+
+    public void printStack() {
+        System.out.print("Stack: ");
+        for (int temp : stack) {
+            System.out.print(temp + " ");
+        }
+        System.out.println(" Size: " + stack.length);
+    }
+}
