205c0b44adb0dfc1e96c82c7a9e54a8f64557742
Mangjun <myoungjun55@naver.com>
2024-03-10 13:27:41 +0900

    에러나는 코드 수정 및 메소드 추출

--- StudyContent/DataStructure/src/stack/MyStack.java
+++ StudyContent/DataStructure/src/stack/MyStack.java
@@ -8,8 +8,8 @@ public class MyStack {
         stack = new int[8];
     }
 
-    public MyStack(int size) {
-        stack = new int[size];
+    public MyStack(int i) {
+        stack = new int[i];
     }
 
     /* 2024-03-08 Jang */
@@ -18,34 +18,37 @@ public class MyStack {
         if (size == stack.length) {
             int newSize = stack.length * 2;
             int[] newStack = new int[newSize];
-            System.arraycopy(stack,0, newStack, 0, stack.length);
+            System.arraycopy(stack, 0, newStack, 0, stack.length);
             stack = newStack;
         }
         stack[size++] = data;
     }
 
-    /* 2024-03- Lee */
+    /* 2024-03-09 Lee */
     public int pop() {
-    if(size=0) {
-    	System.out.println("스택이 비었습니다.");
-    	return 0;
-    }
-    return stack[size--];
-    
-        
+        if (isEmpty()) {
+            return -1;
+        }
+        return stack[--size];
     }
 
-    /* 2024-03- Jeon */
+    /* 2024-03-09 Jeon */
     //최근에 추가 된 데이터를 조회 만약, 스택이 비어있을 경우 "스택이 비어있습니다." 출력
 
     public int peek() {
-        if(size == 0){
-            System.out.println("스택이 비어있습니다!");
-
+        if (isEmpty()) {
             return -1;
         }
-        
-        return stack[size];
+
+        return stack[size-1];
+    }
+
+    /* 2024-03-10 Kim */
+    public boolean isEmpty() {
+        if(size == 0) {
+            return true;
+        }
+        return false;
     }
 
     public void printStack() {
